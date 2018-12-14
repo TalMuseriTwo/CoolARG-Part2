be23f56ef960ee346ba058d3710b63a6d1b3da29
TalMuseriTwo <43849397+TalMuseriTwo@users.noreply.github.com>
2018-12-14 13:07:42 +0200

    Update main.js

--- please/do/NOT/look/here/it/ruins/the/fun/main.js
+++ please/do/NOT/look/here/it/ruins/the/fun/main.js
@@ -1,3 +1,9 @@
-function 2_1() {
-  localtime = new Date();
+function 1() {
+  var localtime = new Date();
+  var year2069 = new Date(2069, 11, 24, 10, 33, 30, 0);
+  if (localtime.getFullYear() == year2069.getFullYear()) {
+      //cookie
+  } else {
+    document.write("<center><font face='Arial'><a dir='rtl'>תקבלו את הרמז הבא עוד 50 שנה.</a></font></center>");
+  }
 }
