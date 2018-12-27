0fd5ca66dabd7d4bae5d9fdf97e189f829ffd631
TalMuseriTwo <43849397+TalMuseriTwo@users.noreply.github.com>
2018-12-27 17:24:29 +0200

    Update main.js

--- please/do/NOT/look/here/it/ruins/the/fun/main.js
+++ please/do/NOT/look/here/it/ruins/the/fun/main.js
@@ -2,8 +2,9 @@ function one() {
   var localtime = new Date();
   var year2069 = new Date(2069, 11, 24, 10, 33, 30, 0);
   if (localtime.getFullYear() == year2069.getFullYear()) {
-      document.write("<center><font face='Arial'><a dir='ltr'>%appdata%/local/google/chrome/user data/default/local storage<br>איפה אפי?</a></font></center>");
-      localstorage.setItem("efi", "SlashFoundEmSlash");
+      document.write("<center><font face='Arial'><a dir='ltr'>you/stupid/no/im/not/whats/9/plus/10/youstupid.html</a></font></center>");
+      //document.write("<center><font face='Arial'><a dir='ltr'>%appdata%/local/google/chrome/user data/default/local storage<br>איפה אפי?</a></font></center>");
+      //localstorage.setItem("efi", "SlashFoundEmSlash");
   } else {
     document.write("<center><font face='Arial'><a dir='rtl'>תקבלו את הרמז הבא עוד 50 שנה.</a></font></center>");
   }
